3a86f78be861f5e0d1dd4fd9a07cacf5c69422c2
Saravanabalagi <saravanabalagi@hotmail.com>
2020-04-17 23:22:43 +0100

    Rename to _old and add explanation in README.md

--- README.md
+++ README.md
@@ -5,10 +5,16 @@ inside your model inference code.
 
 You should be able to see something like
 
-![Screenshot](images/screenshot.png)
+![Screenshot](images/screenshot_old.png)
 
 where 
 - eval has exactly 3 records (at 0, 10 and 20 corresponding to 3 `evaluate`s)
 - train has 20 records (0-9 and 10-19 corresponding to 2 `fit`s with 10 epochs each)
-
  
+For older versions of tensorflow, the hack implemented in [train_and_evaluate_old.py](train_and_evaluate_old.py) 
+where we can pass the summary writer into an argument
+and use it in `call` method to log can be a workaround.
+
+However, from Tensorflow 2.2, we can override Keras `model.fit` using `Model.train_step`. 
+This allows us to write a custom train step and use the same old `model.fit` in an elegant way, 
+but to call our custom train step logic.
--- train_and_evaluate_old.py
+++ train_and_evaluate_old.py
@@ -7,11 +7,10 @@ import os
 
 class MyModel(tf.keras.Model):
 
-    def __init__(self, params):
-        super().__init__()
-        self.dense = tf.keras.layers.Dense(4, name='dense')
+    def __init__(self, params, *args, **kwargs):
         self.params = params
         self.params.summary_writer = params.summary_writer_eval
+        super().__init__(*args, **kwargs)
 
     def fit(self, x=None, y=None, batch_size=None, epochs=1, verbose=1, callbacks=None, validation_split=0.,
             validation_data=None, shuffle=False, class_weight=None, sample_weight=None, initial_epoch=0,
@@ -28,7 +27,13 @@ class MyModel(tf.keras.Model):
         with self.params.summary_writer.as_default():
             tf.summary.scalar("test_scalar", tf.reduce_sum(inputs[0]), step=self.optimizer.iterations)
             tf.summary.scalar("iterations", self.optimizer.iterations, step=self.optimizer.iterations)
-        return self.dense(inputs)
+        print(self._layers)
+        next_layer_inputs = inputs
+        for layer in self._layers:
+            print(layer.__dict__)
+            output = layer(next_layer_inputs)
+            next_layer_inputs = output
+        return next_layer_inputs
 
 
 def main():
@@ -42,16 +47,17 @@ def main():
     params.summary_writer_train = tf.summary.create_file_writer(f"{params.model_dir}/train")
     params.summary_writer_eval = tf.summary.create_file_writer(f"{params.model_dir}/eval")
 
-    model = MyModel(params)
-    model.compile(optimizer=tf.keras.optimizers.Adam(0.001), loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-
     inp = np.random.randint(0, 10, (5, 3)).astype(np.float32)
     inp_valid = np.random.randint(1000, 1100, (5, 3)).astype(np.float32)
     out = np.ones((5, 4), dtype=np.float32)
-
     print(inp, inp_valid)
     print(np.sum(inp, axis=1), np.sum(inp_valid, axis=1))
+
+    input_layer = tf.keras.layers.Input(shape=inp.shape)
+    dense_layer = tf.keras.layers.Dense(4, name='Dense')(input_layer)
+    model = MyModel(inputs=input_layer, outputs=dense_layer, params=params)
+    model.compile(optimizer=tf.keras.optimizers.Adam(0.001), loss='categorical_crossentropy',
+                  metrics=['accuracy'])
 
     model.evaluate(inp_valid, out)
     model.fit(inp, out, epochs=10)
